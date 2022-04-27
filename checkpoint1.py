672efb86a79cdd73182fe44b4445ec51de503bd7
andresbc15 <104385301+andresbc15@users.noreply.github.com>
2022-04-26 22:45:36 -0400

    finished checkpoint1
    added some algos in class files

---
+++ checkpoint1.py
@@ -0,0 +1,89 @@
+'''
+Wesleyan University, COMP 343, Spring 2022
+final project: checkpoint1
+Name: Andres Cojuangco and Peter Fulweiler
+'''
+
+# Helper functions
+
+"""
+Helper functions will iniclude scikit's split_data function, scikit's get_accuracy, get_X_y_data, and
+the get_ID3_accuracy and get_ID3_num_correct functions that we implemented in
+class.
+"""
+
+## Bootstrap
+
+def bootstrapping():
+    """
+    Takes a train dataset and gets x number random dataframe samples with replacement
+    (i.e. some examples can be present in mulitple samples). Each bootstrapped
+    dataset will have y number of examples.
+    """
+
+## Everything for Decision Trees
+
+# Making predictions with only decision trees
+
+def build_decision_trees():
+    """
+    Builds a decision trees from each bootstrapped dataset using a
+    random subspace of the features (x features out of the total number of
+    features).
+    """
+
+def ID3_decision_tree_predictions():
+    """
+    Takes modified ID3 decision tree algorithm to make predictions for each
+    decision tree. This makes predictions for all examples in a dataframe
+    """
+
+def ID3_decision_tree_prediction():
+    """
+    Takes modified ID3 decision tree algorithm to make predictions for each
+    decision tree. This makes a prediction for one example in a dataframe.
+    """
+
+## Everything for Perceptrons
+
+
+# Making predictions with only perceptrons
+
+def perceptron():
+    """
+    Perceptron algorithm that gets learned weights and average number of mistakes
+    per iteration
+    """
+
+def get_perceptron_predictions():
+    """
+    Get's perceptron predictions for all examples in a dataframe
+    """
+
+def get_perceptron_predictions():
+    """
+    Get's perceptron prediction for one example in a dataframe
+    """
+
+## Making predictions with both perceptrons and decision trees
+
+def submodel_combination():
+    """
+    Takes half of the bootstrapped dataframes and makes predictions using decision
+    trees and the other half makes predictions using perceptrons.
+    """
+## Prediction
+
+def final_prediction():
+    """
+    Tallies the predictions of the models and chooses the majority prediction
+    """
+    
+## Data visualization
+
+"""
+Will use a line plot to plot the accuracies of each set of submodels for bagging.
+One set will be all decision trees. One set will be all perceptrons. The last Set
+will be half perceptrons and half decision trees. We will be using two datasets:
+1 linearly separable data set and the other non-linearly separable data set.
+"""
---
+++ decisiontree.py
@@ -0,0 +1,173 @@
+class Node:
+    ''' Class for nodes in decision tree '''
+    def __init__(self, feature_name='', feature_value=None):
+        self.feature_name = feature_name # head if dummy head node, otherwise is feature or label value
+        self.feature_value = feature_value # head if dummy head node, otherwise is feature or label value
+        self.children = None # list of nodes
+
+def print_tree(node, i):
+    ''' Helper function that prints decision tree.
+        Inputs:
+            * node: root node of decision tree to print
+            * i: number of tabs to offset each layer of decision tree
+                 when printing.
+    '''
+    if node.children is None:
+        tabs = i * '\t'
+        print(tabs + str(node.feature_value))
+    else:
+        tabs = i * '\t'
+        print(tabs + str(node.feature_name) + ':' + str(node.feature_value))
+        for child in node.children:
+            print_tree(child, i + 1)
+
+"""
+HELPER FUNCTIONS
+"""
+
+def get_children_from_fvals(df, f):
+    ''' Inputs:
+            * df: dataframe containing data
+            * f: name of current feature being considered
+        Output:
+            * List of child nodes from feature values for f
+    '''
+    fvals = df[f].unique()
+    return [Node(f, v) for v in fvals]
+
+def get_df_num_rows(df):
+    ''' Returns number of rows in dataframe '''
+    return len(df)
+
+def get_df_subset(df, f, fval):
+    ''' Inputs:
+            * df: dataframe containing data
+            * f: name of current feature being considered
+            * fval: one value that f can take on
+        Output:
+            * Dataframe comprising rows of df for which f's value is fval
+    '''
+    df_fval = df[ df[f] == fval ]
+    num_fval = get_df_num_rows(df_fval)
+    return df_fval, num_fval
+
+def get_df_num_labels(df, label):
+    ''' Inputs:
+            * df: dataframe containing data
+            * label: column name in df to use as label
+        Output:
+            * Dictionary, where keys are label value in df_val and values the
+            number of rows in df with that label
+    '''
+    num_label = df[label].value_counts().to_dict()
+    return num_label
+
+def information_gain(df, features, label):
+    ''' Inputs:
+            * df: dataframe containing data
+            * features: current features to consider
+            * label: column name in df to use as label
+        Output:
+            * Feature name on which to split, i.e., feature with the maximum
+              information gain
+    '''
+
+    # How many rows in dataframe are there? Each row is an instance
+    num_instances = get_df_num_rows(df)
+    min_entropy = None
+    split_on = None
+
+    # Iterate through each feature/category to determine
+    # which gives maximum information gain
+    for f in features:
+        sum_entropy = 0
+
+        # Get all values of the current feature to split the dataset
+        for fval in df[f].unique():
+
+            # Get rows of dataframe for which f's value is fval
+            df_fval, num_fval = get_df_subset(df, f, fval)
+
+            # Get counts for each possible label value in df_val
+            num_label_fval = get_df_num_labels(df_fval, label)
+
+            # Calculate entropy
+            entropy = 0
+            for _, num_label in num_label_fval.items():
+                prob = num_label/num_fval
+                entropy += - (prob)*log2(prob)
+            sum_entropy += (num_fval/num_instances) * entropy
+
+        # Get feature with minimum entropy sum
+        # because this feaure has the maximum info gain
+        if min_entropy is None or sum_entropy < min_entropy:
+            min_entropy = sum_entropy
+            split_on = f
+
+    return split_on
+
+"""
+
+ID3 Algorithm
+
+"""
+
+def ID3_decision_tree(df, features, label, max_depth):
+    ''' Inputs
+            * df: dataframe containing data
+            * features: current features to consider
+            * label: column name in df to use as label
+        Output
+            * dtree: root node of trained decision tree
+    '''
+    dtree = Node('root', '')
+    ID3_build_tree(df, features, label, dtree, max_depth)
+    #print_tree(dtree, 0)
+    return dtree
+
+# problem a
+
+root_node = Node('root')
+
+def ID3_build_tree(df, features, label, parent, max_depth):
+    ''' Inputs
+            * df: dataframe containing data
+            * features: current features to consider
+            * label: column name in df to use as label
+            * parent: of type Node
+        Output
+            * a decision tree
+    '''
+
+    #####
+    # Todo: update this function to stop once max depth hit
+    #####
+    depth_count = max_depth
+    # if all features have been used, return most popular label
+    # if there is only one label, also return
+    if len(features) == 0 or len( df[label].unique() ) == 1 or depth_count == 0:
+
+        # Get most frequent label using mode
+        leaf = df[label].mode()[0]
+
+        # Set children to be leaf node containing most frequent label
+        parent.children = [Node(label, leaf)]
+
+    # Otherwise, continue to recurse down the tree
+    else:
+
+        # Get feature with the most info gain
+        feature_to_split_on = information_gain(df, features, label)
+
+        # Values from that feature become the children of the previous node
+        parent.children = get_children_from_fvals(df, feature_to_split_on)
+
+        # Remove the feature we just split on so we don't try to split on it again
+        new_features = features.copy()
+        new_features.remove(feature_to_split_on)
+        #print(new_features)
+        # Recursively call build_tree on each child node
+        for child in parent.children:
+            ID3_build_tree(df[ df[feature_to_split_on] == child.feature_value ],
+                    new_features, label, child,max_depth-1)
+        depth_count -= 1
